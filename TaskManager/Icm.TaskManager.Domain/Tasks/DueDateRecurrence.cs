be1de7f18b320448545f34c23d86bab17b3efe46
Ignacio Calvo <ignacio.calvo@gmail.com>
2014-04-10 01:06:42 +0200

    Cleaning, introducing NodaTime (WIP)

---
+++ TaskManager/Icm.TaskManager.Domain/Tasks/DueDateRecurrence.cs
@@ -0,0 +1,14 @@
+﻿using NodaTime;
+
+namespace Icm.TaskManager.Domain.Tasks
+{
+    public class DueDateRecurrence : Recurrence
+    {
+        public override Task CreateRecurringTask(Task task, ICurrentDateProvider currentDateProvider)
+        {
+            Instant dueDate = task.DueDate.Plus(task.Recurrence.RepeatInterval);
+            Task newTask = task.CopyWithNewDueDate(dueDate, currentDateProvider);
+            return newTask;
+        }
+    }
+}
---
+++ TaskManager/Icm.TaskManager.Domain/Tasks/FinishDateRecurrence.cs
@@ -0,0 +1,19 @@
+﻿using NodaTime;
+
+namespace Icm.TaskManager.Domain.Tasks
+{
+    public class FinishDateRecurrence : Recurrence
+    {
+        public override Task CreateRecurringTask(Task task, ICurrentDateProvider currentDateProvider)
+        {
+            Task newTask = null;
+            if (task.FinishDate.HasValue)
+            {
+                Instant dueDate = task.FinishDate.Value.Plus(task.Recurrence.RepeatInterval);
+                newTask = task.CopyWithNewDueDate(dueDate, currentDateProvider);
+            }
+
+            return newTask;
+        }
+    }
+}
