404e587d0ce87217eb2247d9f2ac14182545c1fa
Ignacio Calvo <ignacio.calvo@frontiersin.org>
2016-12-23 15:16:18 +0100

    F# version started

---
+++ source/Icm.TaskManager.Domain.Fs/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace Icm.TaskManager.Domain.Fs.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("Icm.TaskManager.Domain.Fs")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("Icm.TaskManager.Domain.Fs")>]
+[<assembly: AssemblyCopyright("Copyright ©  2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("bb36d038-6c8c-4dbc-9a1e-c3589a08fc92")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ source/Icm.TaskManager.Domain.Fs/Library1.fs
@@ -0,0 +1,42 @@
+﻿namespace Icm.TaskManager.Domain.Fs
+
+open NodaTime
+
+type Recurrence =
+    | DueDate of Duration
+    | FinishDate of Duration
+
+type Task = {
+    Description: string;
+    CreationDate: Instant;
+    DueDate: Instant;
+    StartDate: Instant option;
+    FinishDate: Instant option;
+    Recurrence: Recurrence option;
+    Priority: int;
+    Notes: string;
+    Labels: string[];
+    Reminders: Instant[];
+}
+
+type Task with
+    static member create desc dueDate now =
+        {
+                Description = desc;
+                CreationDate = now;
+                DueDate = dueDate;
+                StartDate = None;
+                Priority = 3;
+                FinishDate = None;
+                Recurrence = None;
+                Notes = "";
+                Labels = [||];
+                Reminders = [||];
+        }
+
+    member t.isDone = t.FinishDate.IsSome
+
+    member t.recur recurrence newDueDate now =
+        match recurrence with
+        | DueDate d -> { t with DueDate = newDueDate; CreationDate = now }
+        | FinishDate d -> { t with DueDate = newDueDate; CreationDate = now }
